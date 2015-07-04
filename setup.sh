74b1f0ddd939f2db35c81435f272202b401a2265
Parménides GV <parmegv@sdf.org>
2015-07-04 10:54:30 +0200

    Run tests for each repo force build.
    I'm having problems installing leap_pycommon before any other package,
    but since we don't recreate the virtualenv right now, I bypassed the
    issue just building leap_pycommon manually.
    
    Next step: recreate virtualenv, and install everything in that recreation.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Requirements:
+## virtualenv
+## buildbot
+
+base_dir=`pwd`
+virtualenv --python=python2 sandbox
+
+source sandbox/bin/activate
+
+buildbot create-master master
+ln -s $base_dir/master.cfg master/
+ln -s $base_dir/passwords.py master/
+
+slave_name="localhost_slave"
+slave_password=`python2 -c "from passwords import *; print PASSWORDS['$slave_name']"`
+echo $slave_password
+buildslave create-slave slave localhost:9989 $slave_name $slave_password
